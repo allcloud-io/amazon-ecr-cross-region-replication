a621a41ce84b4857da07244ebf98387a3f30a8fb
yakireliyahu1987 <38716819+yakireliyahu1987@users.noreply.github.com>
2020-10-22 11:03:13 +0300

    Added support link to AllCloud's Confluence

--- deployment.py
+++ deployment.py
@@ -9,5 +9,6 @@ deployment_details = [
         'location': './template.yaml',
         'template_constraint': None,
         's3_objects': None,
+        'support_url': "https://allcloud.atlassian.net/wiki/spaces/AWSPRACTICE/pages/897744906/ECR+Cross-Region+Replication"
     }
 ]
